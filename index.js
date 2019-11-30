71da3d43eea0304cff510e81c16e873b1aa167ef
An Phan <me@phanan.net>
2019-11-30 22:10:41 +0100

    Fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const core = require('@actions/core')
 const generateBranchName = () => `refs/heads/changes-${Date.now()}`
 
 async function run () {
-  console.log('x-t', core.getInput('GH_TOKEN'))
+  console.log('x-t', core.getInput('GH_TOKEN')).length
   console.log('x-f', core.getInput('FORKS'))
   const octokit = new github.GitHub(core.getInput('GH_TOKEN'))
   const forks = core.getInput('FORKS')
