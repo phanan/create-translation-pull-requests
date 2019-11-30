2e4e2f8d6bf3f18232e67aac73875b4d343ebbd5
An Phan <me@phanan.net>
2019-11-30 22:24:28 +0100

    Fix

--- dist/index.js
+++ dist/index.js
@@ -7568,26 +7568,35 @@ const core = __webpack_require__(830)
 const generateBranchName = () => `refs/heads/changes-${Date.now()}`
 
 async function run () {
-  console.log('x-t', core.getInput('GH_TOKEN').length)
-  console.log('x-f', core.getInput('FORKS'))
   const octokit = new github.GitHub(core.getInput('GH_TOKEN'))
   const forks = core.getInput('FORKS')
 
-  console.log(JSON.stringify(github.context.payload, undefined, 2))
-  console.log(forks)
+  const owner = github.context.payload.repository.owner.login
+  const repo = github.context.payload.repository.name
+  const branchName = generateBranchName()
 
   const refData = {
-    owner: github.context.payload.repository.owner.login,
-    repo: github.context.payload.repository.name,
-    ref: generateBranchName(),
+    owner,
+    repo,
+    ref: `refs/heads/${branchName}`,
     sha: github.context.payload.after
   }
 
-  console.log(refData)
-
   try {
     const branch = await octokit.git.createRef(refData)
-    console.log(JSON.stringify(branch, undefined, 2))
+
+    const pullData = {
+      owner: 'phanan',
+      repo: 'translation-sample',
+      title: 'Updates on the way!',
+      head: branchName,
+      base: 'master',
+      body: 'Check and merge!',
+      maintainer_can_modify: true
+    }
+
+    const pull = await octokit.pulls.create(pullData)
+    console.log(pull)
   } catch (error) {
     core.setFailed(error.message);
   }
