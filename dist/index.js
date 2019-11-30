edb7eeb6ca9d65daded230e614956afefb42b400
An Phan <me@phanan.net>
2019-11-30 22:02:52 +0100

    Fix

--- dist/index.js
+++ dist/index.js
@@ -7569,6 +7569,7 @@ const generateBranchName = () => `refs/heads/changes-${Date.now()}`
 
 async function run () {
   console.log('x-t', core.getInput('GH_TOKEN'))
+  console.log('x-f', core.getInput('FORKS'))
   const octokit = new github.GitHub(core.getInput('GH_TOKEN'))
   const forks = core.getInput('FORKS')
 
