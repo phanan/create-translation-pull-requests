6bc7f816bba31ac64c985bd1f0e877c793b6f356
An Phan <me@phanan.net>
2019-11-30 21:37:26 +0100

    Trying out

--- index.js
+++ index.js
@@ -3,21 +3,23 @@ const core = require('@actions/core')
 
 const generateBranchName = () => `changes-${Date.now()}`
 
-try {
+(async () => {
   const octokit = new github.GitHub(core.getInput('GH_TOKEN'))
   const forks = core.getInput('FORKS')
 
   console.log(JSON.stringify(github.context.payload, undefined, 2))
   console.log(forks)
 
-  const branch = octokit.git.createRef({
+  const refData = {
     owner: github.context.payload.repository.owner.login,
     repo: github.context.payload.repository.name,
     ref: generateBranchName(),
     sha: github.context.payload.after
-  })
+  }
+
+  console.log(refData)
+
+  const branch = await octokit.git.createRef(refData)
 
   console.log(JSON.stringify(branch, undefined, 2))
-} catch (error) {
-  core.setFailed(error.message);
-}
+}) ()
