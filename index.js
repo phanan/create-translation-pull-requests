d8a97c6b0625a3302f623110fb1f2ddfa0c33356
An Phan <me@phanan.net>
2019-12-01 11:38:23 +0100

    Test if merge triggers push

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const generateBranchName = () => `changes-${Date.now()}`
 async function run () {
   const octokit = new github.GitHub(core.getInput('GH_TOKEN'))
   const forks = core.getInput('FORKS')
+  console.log('forks: ', forks)
 
   const owner = github.context.payload.repository.owner.login
   const repo = github.context.payload.repository.name
@@ -19,20 +20,20 @@ async function run () {
   }
 
   try {
-    const branch = await octokit.git.createRef(refData)
+    // const branch = await octokit.git.createRef(refData)
 
-    const pullData = {
-      owner: 'phanan-forks',
-      repo: 'create-translation-pull-requests',
-      title: 'Updates on the way!',
-      head: `phanan:${branchName}`,
-      base: 'master',
-      body: 'Check and merge!',
-      maintainer_can_modify: true
-    }
+    // const pullData = {
+    //   owner: 'phanan-forks',
+    //   repo: 'create-translation-pull-requests',
+    //   title: 'Latest changes from upstream',
+    //   head: `phanan:${branchName}`,
+    //   base: 'master',
+    //   body: 'Check and merge!',
+    //   maintainer_can_modify: true
+    // }
 
-    const pull = await octokit.pulls.create(pullData)
-    console.log(pull)
+    // const pull = await octokit.pulls.create(pullData)
+    // console.info(`Pull request created at ${pull.data.url}`)
   } catch (error) {
     core.setFailed(error.message);
   }
